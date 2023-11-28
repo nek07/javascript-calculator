966813623f51e3a285e334063834a3b4d6c993c8
nek07 <abylaiooaooa@mail.ru>
2023-11-29 02:16:13 +0600

    implement logic to numbers and .remove-one domjs

--- script.js
+++ script.js
@@ -1,39 +1,26 @@
-$(document).ready(function(){
-let process = '';
-let result = '';
-$('.one').click(function(){
-    process+=1;
-})
-$('.two').click(function(){
-    process += 2;
-});
+document.addEventListener('DOMContentLoaded', function () {
+    let process = '';
+    let result = '';
+    function isZero(){
+        return process.length == 0 ? true : false;
+    }
+    document.querySelectorAll('.num').forEach(function (button) {
+        button.addEventListener('click', function () {
+            let innerValue = this.textContent;
+            process = process + innerValue;
+            document.querySelector('.pole').textContent = process;
+        });
+    });
+   
 
-$('.three').click(function(){
-    process += 3;
-});
-$('.four').click(function(){
-    process += 4;
-});
-
-$('.five').click(function(){
-    process += 5;
-});
-
-$('.six').click(function(){
-    process += 6;
-});
-
-$('.seven').click(function(){
-    process += 7;
-});
-
-$('.eight').click(function(){
-    process += 8;
-});
-
-$('.nine').click(function(){
-    process += 9;
-});
-$('.pole').html(process);
-
+    document.querySelector('.remove-one').addEventListener('click', function() {
+        
+        process = process.slice(0, process.length - 1);
+        document.querySelector('.pole').textContent = process;
+        if(isZero()==true){
+            document.querySelector('.pole').textContent = 0;
+        }
+    });
+    
+    isZero()
 });
