16d67b01a58bd9cc4cd0468279538f4e68827603
nek07 <abylaiooaooa@mail.ru>
2023-11-30 02:04:19 +0600

    operator functions and sqrt, power, 1/x funcs

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
     let process = '';
-    let result = '';
-    function isZero(){
-        return process.length == 0 ? true : false;
-    }
+    let firstNumber = 0;
+    let operator;
+
+
     document.querySelectorAll('.num').forEach(function (button) {
         button.addEventListener('click', function () {
             let innerValue = this.textContent;
@@ -14,28 +14,84 @@ document.addEventListener('DOMContentLoaded', function () {
    
 
     document.querySelector('.remove-one').addEventListener('click', function() {
-        
         process = process.slice(0, process.length - 1);
         document.querySelector('.pole').textContent = process;
-        if(isZero()==true){
-            document.querySelector('.pole').textContent = 0;
-        }
+        clearInput();
     });
     
     document.querySelector('.remove-all').addEventListener('click', function() {
         
         process = ""
-        document.querySelector('.pole').textContent = process;
-        if(isZero()==true){
-            document.querySelector('.pole').textContent = 0;
-        }
+        clearInput();
     });
     document.querySelector('.remove').addEventListener('click', function() {
-        
         process = ""
+        clearInput()
+    });
+    document.querySelectorAll('.operator').forEach(function (button) {
+        button.addEventListener('click', function () {
+        firstNumber = parseInt(process, 10);
+        console.log(firstNumber);
+        operator = this.textContent;
+        process = ""; 
+        clearInput(); 
+    })
+    });
+    
+    
+    document.querySelector('.result').addEventListener('click', function() {
+        let result = concreteOperatorResult(operator,firstNumber,process);
+        console.log(result)
+        if(result!=null){
+            process = result.toString()
+            document.querySelector('.pole').textContent = process;
+        }
+        else {
+            document.querySelector('.pole').textContent = "ERROR";        
+        }
+        
+    });
+    document.querySelector('.sqrt').addEventListener('click', function() {
+        process = Math.sqrt(parseInt(process)).toString();
         document.querySelector('.pole').textContent = process;
+    })
+    document.querySelector('.one-divide-x').addEventListener('click', function() {
+        process = (1/parseInt(process)).toString();
+        document.querySelector('.pole').textContent = process;
+    })
+    document.querySelector('.x-power').addEventListener('click', function() {
+        process = Math.pow(parseInt(process),2).toString();
+        document.querySelector('.pole').textContent = process;
+    })
+    function isZero(){
+        return process.length == 0 ? true : false;
+    }
+    function clearInput() {
         if(isZero()==true){
             document.querySelector('.pole').textContent = 0;
         }
-    });
+   
+    }
+    function concreteOperatorResult(operator,firstNumber,secondNumber) {
+        let result;
+        switch (operator) {
+            case '+':
+                result = parseInt(firstNumber,10) + parseInt(secondNumber,10);
+                break;
+            case '-':
+                result = parseInt(firstNumber,10) - parseInt(secondNumber,10);
+                break;
+            case '/':
+                result = parseInt(firstNumber,10) / parseInt(secondNumber,10);
+                break;
+            case '*':
+                result = parseInt(firstNumber,10) * parseInt(secondNumber,10);
+                break;
+            default:
+                result = null;
+        }
+        return result;
+    }
+    
+
 });
